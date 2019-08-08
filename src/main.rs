44b299b40bce12c70336239153daa68cb3099263
jjorissen52 <jjorissen52@gmail.com>
2019-08-07 19:19:41 -0500

    LOOK AT ME! I am the user now. ᴜwᴜ

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use serenity::{
 
 fn uwu_replacer(original: String) -> String {
     let uwu_letters = "rl";
-    let happy_dance = "uwu YOU SAID UWU ♪┏(・o･)┛♪┗ ( ･o･) ┓♪";
     let mut better_string = String::new();
     for c in original.chars() {
         if uwu_letters.contains(c) {
@@ -21,8 +20,7 @@ fn uwu_replacer(original: String) -> String {
     let best_string= better_string
         .replace("th", "d")
         .replace("ou", "ew")
-        .replace("uwu", happy_dance)
-        .replace("uWu", happy_dance);
+        .replace("qu", "qw");
     String::from(best_string)
 }
 
@@ -30,27 +28,31 @@ struct Handler;
 
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
-        println!("new message incoming!");
+        // println!("{}", msg.content);
         let mut uwud = String::new();
-        if msg.content.starts_with("!uwu") ||  msg.content.starts_with("@uwu") {
+        if !msg.author.bot 
+                && msg.content.contains("uwu") 
+                ||  msg.content.contains(" uWu ")             
+            {
             let mut reply = String::new();
             let ref_of_message = &msg.content;
             match ref_of_message.get(4..) {
                 Some(my_str) => reply += my_str,
                 None => reply += format!("{} uwud!", msg.author.name).as_str()
             }
-            uwud += format!("{} ᴜwᴜ", uwu_replacer(reply)).as_str();
+            uwud += format!("<@{}> uwud! ♪┏(・o･)┛♪┗ ( ･o･) ┓♪ ᴜwᴜ \n {} ᴜwᴜ", msg.author.id, uwu_replacer(reply)).as_str();
 
             // The create message builder allows you to easily create embeds and messages
             // using a builder syntax.
             // This example will create a message that says "Hello, World!", with an embed that has
             // a title, description, three fields, and a footer.
-            let msg = msg.channel_id.send_message(&ctx.http, |m| {
+            let res = msg.channel_id.send_message(&ctx.http, |m| {
                 m.content(uwud);
                 m
             });
+            let _ = msg.delete(&ctx);
 
-            if let Err(why) = msg {
+            if let Err(why) = res {
                 println!("Error sending message: {:?}", why);
             }
         }
